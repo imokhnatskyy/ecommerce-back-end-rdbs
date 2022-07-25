200061c7a5eeadf94b7e36128feae5166eb43950
Igor Mokhnatskyy <98458436+imokhnatskyy@users.noreply.github.com>
2022-07-25 15:55:23 +0300

    clean project

--- src/users/dto/add-role.dto.ts
+++
@@ -1,8 +0,0 @@
-import { IsNumber, IsString } from 'class-validator';
-
-export class AddRoleDto {
-  @IsString({ message: 'value should be string' })
-  readonly value: string;
-  @IsNumber({}, { message: 'value should be integer' })
-  readonly userId: number;
-}
