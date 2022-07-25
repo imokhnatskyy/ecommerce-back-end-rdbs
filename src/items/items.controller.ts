64faf15d64e666e8515598660b8e992550372509
Igor Mokhnatskyy <98458436+imokhnatskyy@users.noreply.github.com>
2022-07-25 15:30:02 +0300

    add all files

---
+++ src/items/items.controller.ts
@@ -0,0 +1,38 @@
+import { Body, Controller, Delete, Get, Param, Post } from '@nestjs/common';
+import { ApiOperation, ApiParam, ApiResponse, ApiTags } from '@nestjs/swagger';
+import { CreateItemDto } from './dto/create-item.dto';
+import { Item } from './items.model';
+import { ItemService } from './items.service';
+
+@ApiTags('Items')
+@Controller('items')
+export class ItemController {
+  constructor(private itemService: ItemService) {}
+
+  @ApiOperation({ summary: 'Create item' })
+  @ApiResponse({ status: 200, type: Item })
+  @Post()
+  create(@Body() itemDto: CreateItemDto): Promise<Item> {
+    return this.itemService.createItem(itemDto);
+  }
+
+  @ApiOperation({
+    summary: 'Item by id',
+  })
+  @ApiResponse({ status: 200, type: Item })
+  @Get('/:id')
+  @ApiParam({ name: 'id' })
+  getProduct(@Param() param: { id: string }): any {
+    return this.itemService.getItemById(param.id);
+  }
+
+  @ApiOperation({
+    summary: 'delete item',
+  })
+  @ApiResponse({ status: 204 })
+  @Delete('/:id')
+  @ApiParam({ name: 'id' })
+  deleteProduct(@Param() param: { id: string }): any {
+    return this.itemService.deleteProduct(param.id);
+  }
+}
---
+++ src/products/products.controller.ts
@@ -0,0 +1,76 @@
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Patch,
+  Post,
+} from '@nestjs/common';
+import { ApiOperation, ApiParam, ApiResponse, ApiTags } from '@nestjs/swagger';
+import { CreateProductDto } from './dto/create-product.dto';
+import { Product } from './products.model';
+import { ProductsService } from './products.service';
+
+@ApiTags('Products')
+@Controller('products')
+export class ProductController {
+  constructor(private productService: ProductsService) {}
+
+  @ApiOperation({ summary: 'Get Products list' })
+  @ApiResponse({ status: 200, type: [Product] })
+  @Get()
+  getAll() {
+    return this.productService.getProducts();
+  }
+
+  @ApiOperation({ summary: 'Create product' })
+  @ApiResponse({ status: 200, type: Product })
+  @Post()
+  create(@Body() productDto: CreateProductDto): Promise<Product> {
+    return this.productService.createProduct(productDto);
+  }
+
+  @ApiOperation({
+    summary: 'Product list by category',
+  })
+  @ApiResponse({ status: 200, type: [Product] })
+  @Get('/category/:id')
+  @ApiParam({ name: 'id' })
+  getCategoryProduct(@Param() param: { id: string }): any {
+    return this.productService.getCategoryProducts(param.id);
+  }
+
+  @ApiOperation({
+    summary: 'Product by id',
+  })
+  @ApiResponse({ status: 200, type: Product })
+  @Get('/:id')
+  @ApiParam({ name: 'id' })
+  getProduct(@Param() param: { id: string }): any {
+    return this.productService.getProductById(param.id);
+  }
+
+  @ApiOperation({
+    summary: 'delete product',
+  })
+  @ApiResponse({ status: 204 })
+  @Delete('/:id')
+  @ApiParam({ name: 'id' })
+  deleteProduct(@Param() param: { id: string }): any {
+    return this.productService.deleteProduct(param.id);
+  }
+
+  @ApiOperation({
+    summary: 'update product',
+  })
+  @ApiResponse({ status: 204 })
+  @Patch('/:id')
+  @ApiParam({ name: 'id' })
+  updaateProduct(
+    @Param() param: { id: string },
+    @Body() productDto: CreateProductDto,
+  ): any {
+    return this.productService.updateProduct(param.id, productDto);
+  }
+}
