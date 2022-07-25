0b53b23b2f4fe22f1d05122096ed8dff4a1a14c6
Igor Mokhnatskyy <imokhnatskyy@tocafootball.com>
2022-07-25 13:40:50 +0300

    Update app.module.ts

--- src/app.module.ts
+++ src/app.module.ts
@@ -20,7 +20,6 @@ import { Card } from './cards/cards.model';
       envFilePath: `.${process.env.NODE_ENV}.env`,
     }),
     SequelizeModule.forRoot({
-      // uri: process.env.DATABASE_URL,
       dialect: 'postgres',
       dialectOptions: {
         ssl: {
@@ -29,7 +28,6 @@ import { Card } from './cards/cards.model';
         },
       },
       host: process.env.DATABASE_HOST,
-      // port: parseInt(process.env.DATABASE_PORT),
       username: process.env.DATABASE_USERNAME,
       password: process.env.DATABASE_PASSWORD,
       database: process.env.DATABASE_NAME,
