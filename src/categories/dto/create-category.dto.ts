62010fd83b372fac5ae17b3ff6d78111d54a6573
Igor Mokhnatskyy <98458436+imokhnatskyy@users.noreply.github.com>
2022-07-25 15:33:25 +0300

    delete

--- src/categories/dto/create-category.dto.ts
+++
@@ -1,6 +0,0 @@
-import { ApiProperty } from '@nestjs/swagger';
-
-export class CreateCategoryDto {
-  @ApiProperty({ example: 'computer', description: 'category name' })
-  readonly category: string;
-}
